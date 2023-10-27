41253ae4985b46dfe6518b3adb486fb42d6092ed
Firdous Ali <firdous.ali.tp@gmail.com>
2023-10-27 19:44:21 +0500

    convert parent class to func

--- src/containers/TestClassComp/index.js
+++ src/containers/TestClassComp/index.js
@@ -1,49 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, TextInput} from 'react-native';
 
 import LevelOne from './LevelOne';
 
-class TestClassComp extends React.Component {
-  constructor(props) {
-    super(props);
+const TestClassComp = props => {
+  const [inputText, setInputValue] = useState('some initial value');
+  const [inputText2, setInputValue2] = useState('some initial value2');
 
-    this.state = {
-      inputText: 'some initial value',
-      inputText2: 'some initial value2',
+  useEffect(() => {
+    return () => {
+      console.log('COMPONENT UNMOUNTED');
     };
-  }
+  }, []);
 
-  componentWillUnmount() {
-    console.log('COMPONENT UNMOUNTED');
-  }
+  console.log('testclass rendered');
 
-  render() {
-    console.log('testclass rendered');
+  return (
+    <View style={{flex: 1}}>
+      <TextInput
+        value={inputText}
+        onChangeText={changedText => {
+          setInputValue(changedText);
+        }}
+        style={{backgroundColor: 'pink', height: 40, padding: 5, margin: 10}}
+      />
 
-    const {inputText, inputText2} = this.state;
+      <TextInput
+        value={inputText2}
+        onChangeText={changedText => {
+          setInputValue2(changedText);
+        }}
+        style={{backgroundColor: 'pink', height: 40, padding: 5, margin: 10}}
+      />
 
-    return (
-      <View style={{flex: 1}}>
-        <TextInput
-          value={this.state.inputText}
-          onChangeText={changedText => {
-            this.setState({inputText: changedText});
-          }}
-          style={{backgroundColor: 'pink', height: 40, padding: 5, margin: 10}}
-        />
-
-        <TextInput
-          value={this.state.inputText2}
-          onChangeText={changedText => {
-            this.setState({inputText2: changedText});
-          }}
-          style={{backgroundColor: 'pink', height: 40, padding: 5, margin: 10}}
-        />
-
-        <LevelOne inputText={inputText} inputText2={inputText2}></LevelOne>
-      </View>
-    );
-  }
-}
+      <LevelOne inputText={inputText} inputText2={inputText2}></LevelOne>
+    </View>
+  );
+};
 
 export default TestClassComp;
