91bd1f13b7138e632fd9117cbdf92ee56bba93a5
Firdous Ali <firdous.ali.tp@gmail.com>
2023-10-27 18:47:35 +0500

    2 3 converted to func

--- src/containers/TestClassComp/LevelThree.js
+++ src/containers/TestClassComp/LevelThree.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-class LevelThree extends React.Component {
-  constructor(props) {
-    super(props);
+const LevelThree = props => {
+  console.log('level 3 rerendered');
 
-    console.log('level three constructor');
-  }
-
-  componentDidMount() {
-    console.log('level three CDM');
-  }
-
-  componentDidUpdate() {
-    console.log('level three CDU');
-  }
-
-  render() {
-    console.log('level 3 rerendered');
-
-    return <View style={{backgroundColor: 'green', flex: 1, margin: 5}}></View>;
-  }
-}
+  return <View style={{backgroundColor: 'green', flex: 1, margin: 5}}></View>;
+};
 
 export default LevelThree;
--- src/containers/TestClassComp/LevelTwo.js
+++ src/containers/TestClassComp/LevelTwo.js
@@ -3,32 +3,16 @@ import {View, Text} from 'react-native';
 
 import LevelThree from './LevelThree';
 
-class LevelTwo extends React.Component {
-  constructor(props) {
-    super(props);
+const LevelTwo = props => {
+  console.log('level 2 rerendered');
 
-    console.log('level two constructor');
-  }
-
-  componentDidMount() {
-    console.log('level two CDM');
-  }
-
-  componentDidUpdate() {
-    console.log('level two CDU');
-  }
-
-  render() {
-    console.log('level 2 rerendered');
-
-    return (
-      <View style={{flex: 1, margin: 5, backgroundColor: 'yellow'}}>
-        <LevelThree></LevelThree>
-        <LevelThree></LevelThree>
-        <LevelThree></LevelThree>
-      </View>
-    );
-  }
-}
+  return (
+    <View style={{flex: 1, margin: 5, backgroundColor: 'yellow'}}>
+      <LevelThree></LevelThree>
+      <LevelThree></LevelThree>
+      <LevelThree></LevelThree>
+    </View>
+  );
+};
 
 export default LevelTwo;
