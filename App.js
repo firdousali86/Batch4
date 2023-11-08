04804c3715683a93c2535bf7aee10c81581d0664
Firdous Ali <firdous.ali.tp@gmail.com>
2023-11-08 20:54:13 +0500

    navigation refactor

--- App.js
+++ App.js
@@ -8,117 +8,17 @@
 import React, {useState, useEffect} from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {
-  DashboardScreen,
-  SettingScreen,
-  HookEffectScreen,
-  TestClassComp,
-  TestContext,
-  LoginScreen,
-  TestUseRef,
-  TestApiScreen,
-  TestReduxScreen,
-} from './src/containers';
-import {Text, View} from 'react-native';
-import {EventRegister} from 'react-native-event-listeners';
 
 import {PersistanceHelper} from './src/helpers';
 import store from './src/store';
 import {Provider} from 'react-redux';
-
-const Stack = createNativeStackNavigator();
+import Navigator from './src/navigator';
 
 function App() {
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
-
-  useEffect(() => {
-    EventRegister.addEventListener('loginEvent', data => {
-      setIsUserLoggedIn(data);
-    });
-
-    PersistanceHelper.getObject('loginDetails')
-      .then(data => {
-        if (data.username && data.password) {
-          setIsUserLoggedIn(true);
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-
-  const getAuthStack = () => {
-    return (
-      <Stack.Group>
-        <Stack.Screen name="Login" component={LoginScreen}></Stack.Screen>
-        <Stack.Screen
-          name="Signup"
-          component={() => (
-            <View>
-              <Text>Signup</Text>
-            </View>
-          )}></Stack.Screen>
-      </Stack.Group>
-    );
-  };
-
-  const getMainStack = () => {
-    return (
-      <Stack.Group>
-        <Stack.Screen
-          name="testReduxScreen"
-          component={TestReduxScreen}
-          options={{title: 'Test Redux'}}
-        />
-        <Stack.Screen
-          name="testApiScreen"
-          component={TestApiScreen}
-          options={{title: 'Test API'}}
-        />
-        <Stack.Screen
-          name="testUseRef"
-          component={TestUseRef}
-          options={{title: 'Test Useref'}}
-        />
-        <Stack.Screen
-          name="testContext"
-          component={TestContext}
-          options={{title: 'Class components'}}
-        />
-        <Stack.Screen
-          name="Dashboard"
-          component={DashboardScreen}
-          options={{title: 'Overview'}}
-        />
-
-        <Stack.Screen
-          name="testClassComp"
-          component={TestClassComp}
-          options={{title: 'Class components'}}
-        />
-
-        <Stack.Screen
-          name="hookEffectScreen"
-          component={HookEffectScreen}
-          initialParams={{city: 'New Delhi', country: 'India'}}
-        />
-
-        <Stack.Screen
-          name="Settings"
-          component={SettingScreen}
-          initialParams={{city: 'New Delhi', country: 'India'}}
-        />
-      </Stack.Group>
-    );
-  };
-
   return (
     <NavigationContainer>
       <Provider store={store}>
-        <Stack.Navigator>
-          {isUserLoggedIn ? getMainStack() : getAuthStack()}
-        </Stack.Navigator>
+        <Navigator />
       </Provider>
     </NavigationContainer>
   );
---
+++ src/navigator/index.js
@@ -0,0 +1,129 @@
+import {useState, useEffect} from 'react';
+import {Text, View, Button} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {
+  DashboardScreen,
+  SettingScreen,
+  HookEffectScreen,
+  TestClassComp,
+  TestContext,
+  LoginScreen,
+  TestUseRef,
+  TestApiScreen,
+  TestReduxScreen,
+  CartScreen,
+} from '../containers';
+import {useNavigation} from '@react-navigation/native';
+import {EventRegister} from 'react-native-event-listeners';
+import {PersistanceHelper} from '../helpers';
+
+const Stack = createNativeStackNavigator();
+
+const Navigator = () => {
+  useEffect(() => {
+    EventRegister.addEventListener('loginEvent', data => {
+      setIsUserLoggedIn(data);
+    });
+
+    PersistanceHelper.getObject('loginDetails')
+      .then(data => {
+        if (data.username && data.password) {
+          setIsUserLoggedIn(true);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const navigation = useNavigation();
+
+  const getAuthStack = () => {
+    return (
+      <Stack.Group>
+        <Stack.Screen name="Login" component={LoginScreen}></Stack.Screen>
+        <Stack.Screen
+          name="Signup"
+          component={() => (
+            <View>
+              <Text>Signup</Text>
+            </View>
+          )}></Stack.Screen>
+      </Stack.Group>
+    );
+  };
+
+  const getMainStack = () => {
+    return (
+      <Stack.Group>
+        <Stack.Screen
+          name="testReduxScreen"
+          component={TestReduxScreen}
+          options={{
+            title: 'Test Redux',
+            headerRight: () => (
+              <Button
+                title={'Cart'}
+                onPress={() => {
+                  navig;
+                }}
+              />
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="cartScreen"
+          component={CartScreen}
+          options={{title: 'Test Cart using redux'}}
+        />
+        <Stack.Screen
+          name="testApiScreen"
+          component={TestApiScreen}
+          options={{title: 'Test API'}}
+        />
+        <Stack.Screen
+          name="testUseRef"
+          component={TestUseRef}
+          options={{title: 'Test Useref'}}
+        />
+        <Stack.Screen
+          name="testContext"
+          component={TestContext}
+          options={{title: 'Class components'}}
+        />
+        <Stack.Screen
+          name="Dashboard"
+          component={DashboardScreen}
+          options={{title: 'Overview'}}
+        />
+
+        <Stack.Screen
+          name="testClassComp"
+          component={TestClassComp}
+          options={{title: 'Class components'}}
+        />
+
+        <Stack.Screen
+          name="hookEffectScreen"
+          component={HookEffectScreen}
+          initialParams={{city: 'New Delhi', country: 'India'}}
+        />
+
+        <Stack.Screen
+          name="Settings"
+          component={SettingScreen}
+          initialParams={{city: 'New Delhi', country: 'India'}}
+        />
+      </Stack.Group>
+    );
+  };
+
+  return (
+    <Stack.Navigator>
+      {isUserLoggedIn ? getMainStack() : getAuthStack()}
+    </Stack.Navigator>
+  );
+};
+
+export default Navigator;
