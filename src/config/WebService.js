8623d44b757e4be2dd6b5b8383b2159a56f5f7e1
Firdous Ali <firdous.ali.tp@gmail.com>
2023-11-08 21:38:21 +0500

    error handling

--- src/config/WebService.js
+++ src/config/WebService.js
@@ -1,3 +1,15 @@
 export const kApiUrlEndpoint = 'https://jsonplaceholder.typicode.com';
 
 export const kApiTodos = '/todos';
+
+export const ERROR_NETWORK_NOT_AVAILBLE = {
+  title: 'Oops!',
+  message:
+    'Slow or no internet connection. Please check your internet settings',
+};
+
+export const ERROR_WRONG_CREDENTIALS = {
+  title: 'Oops!',
+  message:
+    'Credentials doesnt match. Please try again with correct credentials.',
+};
