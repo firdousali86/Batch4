cf4b015448698912a4f61c9a4c784a7aac1dbe95
Firdous Ali <firdous.ali.tp@gmail.com>
2023-11-23 20:33:32 +0500

    bug fix

--- src/components/MapViewControl/index.js
+++ src/components/MapViewControl/index.js
@@ -1,8 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import MapView, {PROVIDER_GOOGLE, Marker, Callout} from 'react-native-maps';
 
 const MapViewControl = props => {
+  const [isMapReady, setIsMapReady] = useState(false);
+
   return (
     <View style={{flex: 1}}>
       <MapView
@@ -11,41 +13,48 @@ const MapViewControl = props => {
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         showsUserLocation
+        onMapReady={() => {
+          setIsMapReady(true);
+        }}
         region={{
           latitude: 37.78825,
           longitude: -122.4324,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}>
-        {props?.markers.map(thisEl => {
-          return (
-            <Marker
-              draggable
-              title={thisEl.title}
-              description={'this is the description'}
-              coordinate={thisEl}>
-              <View
-                style={{
-                  width: 40,
-                  height: 40,
-                  backgroundColor: 'grey',
-                  borderRadius: 20,
-                  justifyContent: 'center',
-                  alignItems: 'center',
+        {isMapReady === true &&
+          props?.markers.map(thisEl => {
+            return (
+              <Marker
+                draggable
+                title={thisEl.title}
+                description={'this is the description'}
+                coordinate={{
+                  latitude: parseFloat(thisEl.latitude),
+                  longitude: parseFloat(thisEl.longitude),
                 }}>
-                <Text>F</Text>
-              </View>
-              <Callout>
                 <View
                   style={{
-                    width: 300,
-                    height: 150,
-                    backgroundColor: 'red',
-                  }}></View>
-              </Callout>
-            </Marker>
-          );
-        })}
+                    width: 40,
+                    height: 40,
+                    backgroundColor: 'grey',
+                    borderRadius: 20,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Text>F</Text>
+                </View>
+                <Callout>
+                  <View
+                    style={{
+                      width: 300,
+                      height: 150,
+                      backgroundColor: 'red',
+                    }}></View>
+                </Callout>
+              </Marker>
+            );
+          })}
       </MapView>
     </View>
   );
